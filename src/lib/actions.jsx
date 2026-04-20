16023a1e23a7c8037fed3bcb4966286df3c646cc
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-20 19:24:11 +0600

    added task via form and displayed it

--- src/lib/actions.jsx
+++ src/lib/actions.jsx
@@ -1,9 +1,32 @@
+import { revalidatePath } from "next/cache";
+import { postTask } from "./task";
+
 export const AddATask = async (formData) =>{
     'use server'
 
-    const name = formData.get('name');
-    console.log("adding task: ", formData);
+    // lengthy way
+    // const title = formData.get('title');
+    // const description = formData.get('description');
+    // const status = formData.get('status');
+    // const priority = formData.get('priority');
+    // const assignedTo = formData.get('assignedTo');
 
+    // const newTask = {
+    //     title,
+    //     description,
+    //     status,
+    //     priority,
+    //     assignedTo
+    // }
 
+    // fast (lazy) way
 
+    const newTask = Object.fromEntries(formData.entries());
+    console.log(newTask);
+
+    const res = await postTask(newTask);
+    if(res.ok){
+        revalidatePath('/tasks');
+    }
+    return res;
 }
--- src/lib/task.js
+++ src/lib/task.js
@@ -3,3 +3,9 @@ import task from '../data/tasks.json';
 export const getTasks = async () =>{
     return task;
 }
+
+export const postTask = async (newTask) => {
+    newTask.id = task.length + 1;
+    task.push(newTask);
+    return {ok: true, message: 'task added successfully'};
+}
